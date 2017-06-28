be2e9d5a27048563367ca4dde381c8bd0b92a876
Trevor Jex <jextrevor@gmail.com>
2017-06-28 13:19:34 -0600

    Systems work

--- systems.py
+++ systems.py
@@ -24,6 +24,9 @@ def watch(name,value):
         return function
     return real_decorator
 main = Main()
+def attach(system1,system2):
+    system1.attach(type(system2).__name__,system2)
+    system2.attach(type(system1).__name__,system1)
 class System:
     def __init__(self):
         self.watches = {}
@@ -36,11 +39,7 @@ class System:
                     value.observe(getattr(self,function).watch[1],getattr(self,function))
             except AttributeError:
                 pass"""
-        try:
-            for function in self.watches[attr]:
-                threading.Thread(target=function,args=(value,)).start()
-        except KeyError:
-            self.watches[attr] = []
+        notify(attr)
     def attach(self,attr,value):
         try:
             self.__dict__[attr].append(value)
@@ -69,51 +68,12 @@ class System:
         except KeyError:
             self.watches[value] = []
             self.watches[value].append(function)
-class PowerConduit(System):
-    def __init__(self,name,priority):
-        System.__init__(self)
-        self.name = name
-        self.power = 0
-        self.priority = 0
-    def requestPower(self,power):
-        mag = power - self.power
-        if self.Power.requestPower(mag):
-            self.power = power
-            return True
-        return False
-    def changePriority(self,priority):
-        self.priority = priority
-        return True
-    def lowerPower(self,request):
-        if request > self.power:
-            t = self.power
-            self.power = 0
-            return t
-        else:
-            self.power -= request
-            return request
-class Power(System):
-    def __init__(self):
-        System.__init__(self)
-        self.total = 0
-        self.in_use = 0
-        self.Source = []
-    @watch("Source","available")
-    def power_changed(self,value):
-        pass
-class Lights(System):
-    def __init__(self)
-        System.__init__(self)
-        self.brightness = 0
-    def adjustLights(self,value):
-        if self.Conduit.power < value:
-            return False
-        self.brightness = value
-        return True
-    @watch("Conduit","power")
-    def power_changed(self,value):
-        if value > self.brightness:
-            self.brightness = value
+    def notify(self,attr):
+        try:
+            for function in self.watches[attr]:
+                threading.Thread(target=function,args=()).start()
+        except KeyError:
+            self.watches[attr] = []
 class Alerts(System):
     def __init__(self):
         System.__init__(self)
@@ -121,6 +81,64 @@ class Alerts(System):
     def changeStatus(self,status):
         self.status = status
         return True
+class Lights(System):
+    def __init__(self):
+        System.__init__(self)
+        self.brightness = 0
+    def changeBrightness(self,brightness):
+        if brightness > self.Power.lights:
+            return "Insufficient power."
+        self.brightness = brightness
+        return True
+    @watch("Power","lights")
+    def power_changed(self):
+        if brightness > self.Power.lights:
+            self.brightness = self.Power.lights
+#4 things need to happen:
+#The actual coordinates need to be set. Doesn't require sensors.
+#The target rotation needs to be calculated. Requires sensors to determine current position.
+#The target rotation needs to be reached. Requires sensors to determine current direction.
+class Course(System):
+    def __init__(self):
+        System.__init__(self)
+        self.x = 0
+        self.y = 0
+        self.z = 0
+        self.status = False
+    def changeCourse(self,x,y,z):
+        #First, check sensors.
+        self.x = x
+        self.y = y
+        self.z = z
+        #Now, calculate the target direction.
+        #First, subtract current position (obtained through sensors) from target position.
+        #Next, calculate the magnitude using Pythagorean theorem.
+        #Next, divide all components by magnitude.
+        #Next, calculate pitch by taking the inverse sine of Z.
+        #Next, divide X by the cosine of pitch, then take the inverse cosine to find the yaw.
+        #Also need to code in exceptions.
+        return True
+    def changeStatus(self,status):
+        if status == True and self.Repair.course > some config value:
+            self.status = True
+            return True
+        if status == True:
+            return "System damaged."
+        if status == False:
+            self.status = False
+            return True
+    @watch("Repair","course")
+    def damage_changed(self):
+        if self.Repair.course < some config value:
+            self.status = False
+    def activity(self):
+        #Orient the ship to the target rotations.
+#The target velocity needs to be calculated. This involves calculating the ship's current pitch and yaw and making a directional vector, then multiplying it by the target speed.
+#Next, the difference between the target velocity and the current velocity needs to be calculated.
+#Next, the engine needs to normalize the difference, and apply a force along it.
+class Engine(System):
+    def __init__(self):
+        System.__init__(self)
 s1 = Alerts()
 s2 = Alerts()
 s1.attach("Alerts",s2)
