574e32268f0d62c8d251a205e9fc243abb9676a0
Trevor Jex <jextrevor@gmail.com>
2017-07-01 22:04:48 -0600

    more work

--- systems.py
+++ systems.py
@@ -74,6 +74,7 @@ class System:
                 threading.Thread(target=function,args=()).start()
         except KeyError:
             self.watches[attr] = []
+#Resilient alert system.
 class Alerts(System):
     def __init__(self):
         System.__init__(self)
@@ -81,6 +82,7 @@ class Alerts(System):
     def changeStatus(self,status):
         self.status = status
         return True
+#LED ship-wide lighting system.
 class Lights(System):
     def __init__(self):
         System.__init__(self)
@@ -98,6 +100,7 @@ class Lights(System):
 #The actual coordinates need to be set. Doesn't require sensors.
 #The target rotation needs to be calculated. Requires sensors to determine current position.
 #The target rotation needs to be reached. Requires sensors to determine current direction.
+#Sensor-based positioning and navigating system.
 class Course(System):
     def __init__(self):
         System.__init__(self)
@@ -105,11 +108,11 @@ class Course(System):
         self.y = 0
         self.z = 0
         self.status = False
-    def changeCourse(self,x,y,z):
-        #First, check sensors.
+    def setCourse(self,x,y,z):
         self.x = x
         self.y = y
         self.z = z
+        #Check sensors.
         #Now, calculate the target direction.
         #First, subtract current position (obtained through sensors) from target position.
         #Next, calculate the magnitude using Pythagorean theorem.
@@ -117,6 +120,18 @@ class Course(System):
         #Next, calculate pitch by taking the inverse sine of Z.
         #Next, divide X by the cosine of pitch, then take the inverse cosine to find the yaw.
         #Also need to code in exceptions.
+        #Set the thrusters to go in that direction.
+        return True
+    def reorient(self):
+        #Check sensors.
+        #Now, calculate the target direction.
+        #First, subtract current position (obtained through sensors) from target position.
+        #Next, calculate the magnitude using Pythagorean theorem.
+        #Next, divide all components by magnitude.
+        #Next, calculate pitch by taking the inverse sine of Z.
+        #Next, divide X by the cosine of pitch, then take the inverse cosine to find the yaw.
+        #Also need to code in exceptions.
+        #Set the thrusters to go in that direction.
         return True
     def changeStatus(self,status):
         if status == True and self.Repair.course > some config value:
@@ -131,45 +146,75 @@ class Course(System):
     def damage_changed(self):
         if self.Repair.course < some config value:
             self.status = False
-    def activity(self):
-        pass
-        #Orient the ship to the target rotations.
+#Power transformer, manager and switcher.
 class Power(System):
     def __init__(self):
         System.__init__(self)
+#Large lithium-ion power banks.
 class Battery(System):
     def __init__(self):
         System.__init__(self)
+#Nuclear fusion-based generator.
 class Generator(System):
     def __init__(self):
         System.__init__(self)
+#Bot-based basic repairing and replacing system.
 class Repair(System):
     def __init__(self):
         System.__init__(self)
+        self.course = 100
+        self.generator = 100
+        self.battery = 100
+        self.power = 100
+        self.sensors = 100
+        self.radar = 100
+        self.radio = 100
+        self.targeting = 100
+        self.lasers = 100
+        self.security = 100
+        self.transporter = 100
+        self.thrusters = 100
+        self.engine = 100
+#Positioning using long-range satellite transmissions, and data analysis.
 class Sensors(System):
     def __init__(self):
         System.__init__(self)
+#Infrared based positioning and locating system.
 class Radar(System):
     def __init__(self):
         System.__init__(self)
+#Basic message transciever.
 class Radio(System):
     def __init__(self):
         System.__init__(self)
+#Semi-accurate positioning and aiming system for use with lasers.
 class Targeting(System):
     def __init__(self):
         System.__init__(self)
+#Short, high energy laser based weaponry system, powered through capacitor bays.
 class Lasers(System):
     def __init__(self):
         System.__init__(self)
+#Intercom system for security teams.
 class Security(System):
     def __init__(self):
         System.__init__(self)
+#Shuttle-based transporter system
 class Transporter(System):
+    def __init__(self):
+        System.__init__(self)
+#Using sensors, a high precision orienting system.
+class Thrusters(System):
+    def __init__(self):
+        System.__init__(self)
+#Switchable highly reflective ship armour.
+class Armor(System):
     def __init__(self):
         System.__init__(self)
 #The target velocity needs to be calculated. This involves calculating the ship's current pitch and yaw and making a directional vector, then multiplying it by the target speed.
 #Next, the difference between the target velocity and the current velocity needs to be calculated.
 #Next, the engine needs to normalize the difference, and apply a force along it.
+#Multi-direcitonal ion thruster propulsion system.
 class Engine(System):
     def __init__(self):
         System.__init__(self)
