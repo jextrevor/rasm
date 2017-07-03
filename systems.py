e8912224ac538ba62ca53865bdfaf4c0923f7dfe
Trevor Jex <jextrevor@gmail.com>
2017-07-03 15:38:53 -0600

    work on objects

--- systems.py
+++ systems.py
@@ -1,12 +1,15 @@
 import threading
 import time
-from multiprocessing import Process
+import os
 class Main:
     def __init__(self):
         self.activities = []
+        self.objects = []
     def start(self):
         for function in self.activities:
-            p = Process(target = function, args=())
+            def f():
+                function.run()
+            p = threading.Thread(target = f, args=())
             p.start()
 def watch(name,value):
     def real_decorator(function):
@@ -25,12 +28,12 @@ def watch(name,value):
     return real_decorator
 main = Main()
 def attach(system1,system2):
-    system1.attach(type(system2).__name__,system2)
-    system2.attach(type(system1).__name__,system1)
+    system1.attach(system2.__class__.__name__,system2)
+    system2.attach(system1.__class__.__name__,system1)
 class System:
     def __init__(self):
         self.watches = {}
-        main.activities.append(self.run)
+        main.activities.append(self)
     def __setattr__(self,attr,value):
         self.__dict__[attr] = value
         """for function in [a for a in dir(self) if not a.startswith('__')]:
@@ -39,11 +42,12 @@ class System:
                     value.observe(getattr(self,function).watch[1],getattr(self,function))
             except AttributeError:
                 pass"""
-        notify(attr)
+        self.notify(attr)
     def attach(self,attr,value):
         try:
             self.__dict__[attr].append(value)
-        self.__dict__[attr] = value
+        except (KeyError,AttributeError):
+            self.__dict__[attr] = value
         for function in [a for a in dir(self) if not a.startswith('__')]:
             try:
                 if getattr(self,function).watch[0] == attr:
@@ -74,6 +78,98 @@ class System:
                 threading.Thread(target=function,args=()).start()
         except KeyError:
             self.watches[attr] = []
+class Object(System):
+    def __init__(self,x,y,z,name):
+        System.__init__(self)
+        main.objects.append(self)
+        self.cargoes = []
+        self.x = x
+        self.y = y
+        self.z = z
+        self.vx = 0
+        self.vy = 0
+        self.vz = 0
+        self.ax = 0
+        self.ay = 0
+        self.az = 0
+        self.name = name
+        self.counter = 0
+    def activity(self):
+        self.x += self.vx*self.delta + 0.5*self.ax*self.delta*self.delta
+        self.y += self.vy*self.delta + 0.5*self.ay*self.delta*self.delta
+        self.z += self.vz*self.delta + 0.5*self.az*self.delta*self.delta
+        self.vx = self.vx + self.ax*self.delta
+        self.vy = self.vy + self.ay*self.delta
+        self.vz = self.vz + self.az*self.delta
+        self.counter += 1
+    def test(self,x):
+        self.x = x
+    def damage(self,factor):
+        pass
+class Shuttle(Object):
+    def __init__(self,x,y,z,name):
+        Object.__init__(self,x,y,z,name)
+        self.location = None
+    def activity(self):
+        if self.location != None:
+            self.x = self.location.x
+            self.y = self.location.y
+            self.z = self.location.z
+            #self.vx = 0
+            self.vy = 0
+            self.vz = 0
+            self.ax = 0
+            self.ay = 0
+            self.az = 0
+        else:
+            Object.activity(self)
+    def dock(self,location):
+        if self.location != None:
+            return "Detach first."
+        if distance(self,location) < 50*50:
+            self.location = location
+            self.location.cargoes.append(self)
+            return True
+        return "Distance too far."
+    def undock(self):
+        print("undocking")
+        self.location.cargoes.remove(self)
+        self.location = None
+        print(self.location)
+class Cargo(System):
+    def __init__(self,location,name):
+        System.__init__(self)
+        self.cargoes = []
+        self.location = location
+        self.location.cargoes.append(self)
+        self.name = name
+    def transfer(self,location):
+        if distance(self.location,location) < 50*50:
+            self.location.cargoes.remove(self)
+            self.location = location
+            self.location.cargoes.append(self)
+            return True
+        return "Distance too far."
+def distance(l1,l2):
+    try:
+        xl1 = l1.x
+        yl1 = l1.y
+        zl1 = l1.z
+    except AttributeError:
+        return distance(l1.location,l2)
+    try:
+        xl2 = l2.x
+        yl2 = l2.y
+        zl2 = l2.z
+    except AttributeError:
+        return distance(l1,l2.location)
+    return (xl2-xl1)*(xl2-xl1)+(yl2-yl1)*(yl2-yl1)+(zl2-zl1)*(zl2-zl1)
+class Ship(Object):
+    def __init__(self,x,y,z,callsign,nation):
+        Object.__init__(self,x,y,z,callsign)
+        self.nation = nation
+    def damage(self,factor):
+        pass
 #Resilient alert system.
 class Alerts(System):
     def __init__(self):
@@ -134,7 +230,7 @@ class Course(System):
         #Set the thrusters to go in that direction.
         return True
     def changeStatus(self,status):
-        if status == True and self.Repair.course > some config value:
+        if status == True and self.Repair.course > 50:
             self.status = True
             return True
         if status == True:
@@ -144,7 +240,7 @@ class Course(System):
             return True
     @watch("Repair","course")
     def damage_changed(self):
-        if self.Repair.course < some config value:
+        if self.Repair.course < 50:
             self.status = False
 #Power transformer, manager and switcher.
 class Power(System):
@@ -179,6 +275,9 @@ class Repair(System):
 class Sensors(System):
     def __init__(self):
         System.__init__(self)
+        self.x = 0
+        self.y = 0
+        self.z = 0
 #Infrared based positioning and locating system.
 class Radar(System):
     def __init__(self):
@@ -199,6 +298,11 @@ class Lasers(System):
 class Security(System):
     def __init__(self):
         System.__init__(self)
+        self.SecurityTeam = []
+class SecurityTeam(Cargo):
+    def __init__(self,location,name):
+        Cargo.__init__(self,location,name)
+        self.status = 0 #Standing By
 #Shuttle-based transporter system
 class Transporter(System):
     def __init__(self):
@@ -218,9 +322,26 @@ class Armor(System):
 class Engine(System):
     def __init__(self):
         System.__init__(self)
-s1 = Alerts()
-s2 = Alerts()
-s1.attach("Alerts",s2)
+#ship = Ship(0,0,0,"Enterprise","USS")
+#ship2 = Ship(0,49.9,0,"Enterprise2","USS")
+#c1 = Cargo(ship,"Engine Room")
+#c2 = Cargo(ship,"Shuttle Bay 1")
+#s1 = Security()
+#s2 = SecurityTeam(c1,"Team1")
+#s3 = Shuttle(0,0,0,"Shuttle1")
+#s3.dock(ship)
+#attach(s2,s1)
+#attach(s1,ship)
+#ship.Security.SecurityTeam[0].transfer(s3)
+#print(ship.Security.SecurityTeam[0].location.name)
+#s3.undock()
+#print(s3.location)
+#s3.vx = 1000
+#time.sleep(5)
+#print(s3.x)
+#s3.vx = 0
+#ship.Security.SecurityTeam[0].transfer(c1)
+#print(ship.Security.SecurityTeam[0].location.name)
 #How to handle power?
 #main.start()
 #Each system stores other systems it needs as attributes.
